2669c413f27b7fa8c1c5eb4559cd0ad749c617e3
picnic88 <ssemilife@gmail.com>
2025-12-05 11:29:19 +0900

    merge

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import H from './H.jsx';
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <H />
+    </>
+  )
+}
+
+export default App
