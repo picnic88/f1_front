6f2de69303d1d1307ed4c1b758006f7b9d4e258f
seryeong1230 <sergo1234@naver.com>
2025-12-08 21:40:55 +0900

    주석 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,24 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // 여기서 프록시 설정을 합니다
+    // 여기서 프록시 설정을 함
     proxy: {
-      // 1. API 요청 (/api 로 시작하는 것들)
+      // API 요청 (/api 로 시작하는 것들)
       '/api': {
         target: 'http://localhost:8081',
         changeOrigin: true,
       },
-      // 2. 로그인 요청
+      //로그인 요청
       '/loginProc': {
         target: 'http://localhost:8081',
         changeOrigin: true,
       },
-      // 3. 회원가입 요청
+      //회원가입 요청
       '/joinProc': {
         target: 'http://localhost:8081',
         changeOrigin: true,
       },
-      // 4. 로그아웃 요청
+      //로그아웃 요청
       '/logout': {
         target: 'http://localhost:8081',
         changeOrigin: true,
